25446d5c4807a3823e45246a50e69deff319548f
Maaz Ghani <maazghani+github@gmail.com>
2023-05-22 20:21:39 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 SERVER_URL=${SERVER_URL:-http://localhost:5003}
-sed -i 's|server.url:.*|server.url: "'$SERVER_URL'"|g' openapi.yaml
+sed -i 's|server.url:.*|server.url: "'$SERVER_URL'"|g' openapi.yaml .well-known/ai-plugin.json
 python main.py
